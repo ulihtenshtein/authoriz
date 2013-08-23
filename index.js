ca887595b4af1db984a36f573ca477bb8a329ec1
ulrix lihtenshtein <ulihtenshtein@gmail.com>
2013-08-23 10:55:03 +0700

    work version

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ var static = require('node-static');
 var file = new static.Server('.');
 
 http.createServer(function (req, res) {
-    try {
+    try {// проверяем путь
 	if (req.url == '/info') {
-		console.log(req.headers);
+	// проверяем авторизацию
        if(req.headers['authorization'] == 'password') {
-		res.writeHead(200, {
-				      'Content-Type': 'text/plain; charset=utf-8',
-				       'Cache-Control':'no-cache',
-					'Access-Control-Allow-Origin':'*'
-});
+		res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
 
 		var now = new Date();
 		var timeStr = now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
